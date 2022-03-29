daf0ee6cb1fa5cc485a3b9417a7b9fe7928b074c
ahmadadejumo <adejumoahmad@gmail.com>
2022-03-30 00:51:33 +0100

    add text and styling

--- screens/SplashScreen1.jsx
+++ screens/SplashScreen1.jsx
@@ -1,14 +1,21 @@
 import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
 import React from "react";
+import tw from "twrnc";
 
 const SplashScreen1 = () => {
   return (
     <SafeAreaView>
-      <View>
+      <View style={[tw`pt-30`]}>
         <Image
           source={require("../assets/boy.png")}
-          style={{ width: 400, height: 400 }}
+          style={[{ width: 400, height: 400 }]}
         />
+      </View>
+      <View>
+        <Text style={[tw`text-center font-bold text-2xl pt-5`]}>
+          Find Your Favourite Lesson
+        </Text>
+        <View style={[tw`border-2 mx-42 mt-1 border-emerald-400`]} />
       </View>
     </SafeAreaView>
   );
