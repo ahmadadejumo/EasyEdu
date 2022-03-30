1d822bdee0367f2c760fc1759b7898eb80529501
ahmadadejumo <adejumoahmad@gmail.com>
2022-03-30 11:03:32 +0100

    add button and styling

--- screens/Signin.jsx
+++ screens/Signin.jsx
@@ -4,13 +4,14 @@ import {
   KeyboardAvoidingView,
   TextInput,
   Image,
+  TouchableOpacity,
 } from "react-native";
 import React from "react";
 import tw from "twrnc";
 
 const Signin = () => {
   return (
-    <KeyboardAvoidingView style={[tw`flex-1 items-center pt-20`]}>
+    <KeyboardAvoidingView style={[tw`flex-1 items-center pt-14`]}>
       <Text style={[tw`text-2xl font-medium`]}>Sign In</Text>
       <Image
         source={require("../assets/signin.png")}
@@ -21,17 +22,27 @@ const Signin = () => {
         <TextInput
           placeholder="Enter email or phone number"
           style={[
-            tw`w-[300px] h-[50px] border border-gray-400 rounded-xl px-4 mt-3 text-base`,
+            tw`w-[350px] h-[50px] border border-gray-400 rounded-xl px-4 mt-3 text-base`,
           ]}
         />
         <Text style={[tw`text-lg font-medium pt-5`]}>Password</Text>
         <TextInput
           placeholder="Enter password"
           style={[
-            tw`w-[300px] h-[50px] border border-gray-400 rounded-xl px-4 mt-3 text-base`,
+            tw`w-[350px] h-[50px] border border-gray-400 rounded-xl px-4 mt-3 text-base`,
           ]}
         />
       </View>
+      <Text style={[tw`pt-5 text-base font-medium text-emerald-500`]}>
+        Forgot password?
+      </Text>
+      <TouchableOpacity
+        style={[
+          tw`h-[55px] w-[350px] mt-5 shadow-2xl shadow-emerald-500 bg-emerald-500 flex justify-center items-center rounded-3xl`,
+        ]}
+      >
+        <Text style={[tw`text-xl text-white`]}>Sign In</Text>
+      </TouchableOpacity>
     </KeyboardAvoidingView>
   );
 };
